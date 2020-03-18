4e50e4f5e16cc8a92eae97479c7254e9c2facca4
Bgt <jordanbouget@gmail.com>
2020-03-18 14:49:16 +0100

    Récupération de la liste des topos

--- src/app/entities/topo/topo.component.ts
+++ src/app/entities/topo/topo.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { TopoService } from './topo.service';
+import { HttpResponse } from '@angular/common/http';
+import { ITopo } from '../../shared/model/topo.model';
+
+type EntityResponseType = HttpResponse<ITopo>;
+type EntityArrayResponseType = HttpResponse<ITopo[]>;
 
 @Component({
   selector: 'app-topo',
@@ -7,9 +13,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopoComponent implements OnInit {
 
-  constructor() { }
+  topos: ITopo[];
+
+  constructor(private topoService: TopoService) {
+    this.topos = [];
+  }
+
+  loadAll() {
+    this.topoService.getAllTopos().subscribe((res: EntityArrayResponseType ) => this.topos = res.body);
+  }
 
   ngOnInit() {
+    this.loadAll();
   }
 
 }
