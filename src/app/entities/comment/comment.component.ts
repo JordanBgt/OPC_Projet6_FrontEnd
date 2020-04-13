af30c3ee04a8d2f36ad0f62fc7b600bfd5bc7163
Bgt <jordanbouget@gmail.com>
2020-04-13 15:53:47 +0200

    Implémentation liste des commentaires dans la vue détaillée d'un spot
    Pas encore de css, ni création/modification/suppression d'un commentaire

--- src/app/entities/comment/comment.component.ts
+++ src/app/entities/comment/comment.component.ts
@@ -1,15 +1,48 @@
-                                                  import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IComment } from '../../shared/model/comment.model';
+import { CommentService } from './comment.service';
+import { HttpResponse } from '@angular/common/http';
+import { ITEMS_PER_PAGE } from '../../../../app.constants';
+
+type EntityArrayResponseType = HttpResponse<IComment[]>;
+type EntityResponseType = HttpResponse<IComment>;
 
 @Component({
-  selector: 'app-commentaire',
+  selector: 'app-comment',
   templateUrl: './comment.component.html',
   styleUrls: ['./comment.component.scss']
 })
 export class CommentComponent implements OnInit {
 
-  constructor() { }
+  @Input() spotId: number;
+  comments: IComment[];
+  size: number;
+  page: number;
+  totalPages: number;
 
-  ngOnInit() {
+  constructor(private commentService: CommentService) {
+    this.comments = [];
+    this.size = ITEMS_PER_PAGE;
+    this.page = 0;
   }
 
+  ngOnInit() {
+    this.loadAll();
+  }
+
+  loadAll() {
+    this.commentService.getAllComments({spotId: this.spotId}).subscribe((res: EntityArrayResponseType) => this.paginateComments(res.body));
+  }
+
+  paginateComments(data: any) {
+    this.totalPages = data.totalPages;
+    for (const comment of data.content) {
+      this.comments.push(comment);
+    }
+  }
+
+  loadPage(page) {
+    this.page = page;
+    this.loadAll();
+  }
 }
